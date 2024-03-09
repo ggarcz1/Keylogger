124272c5e1e5763b72a09ad7bcf231c7e54036b7
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2024-03-08 19:50:48 -0600

    Update parse_keys.py

--- parse_keys.py
+++ parse_keys.py
@@ -22,35 +22,35 @@ key_backspace = re.compile(r'Key.backspace')
 # https://condor.depaul.edu/sjost/lsp121/documents/ascii-npr.htm
 
 # TODO:
+def log(value) -> None:
+    f = open('Parse_logs.txt' 'a')
+    f.write(value)
+    f.close()
 
 output_string = ''
 f = open('keylogs.txt', 'r')
 for line in f:
+    value = character.findall(line)
+    space_search = key_space.findall(line)
+    enter_search = key_enter.findall(line)
+    backspace_search = key_backspace.findall(line)
 
-    try:
-        value = character.findall(line)
-        space_search = key_space.findall(line)
-        enter_search = key_enter.findall(line)
-        backspace_search = key_backspace.findall(line)
+    # space
+    if (len(space_search) != 0):
+        output_string += ' '
+    # enter
+    elif(len(enter_search) != 0):
+        output_string += '\n'
+    # backspace
+    elif(len(backspace_search) != 0):
+        output_string += '<BSPACE>'
+    # char or numerical value
+    elif(len(value) != 0):
+        output_string += value[0]
+    else:
+        # remove new line
+        # output_string += line[25:][:-1]
+        output_string += line[25:]
 
-        # space
-        if (len(space_search) != 0):
-            output_string += ' '
-        # enter
-        elif(len(enter_search) != 0):
-            output_string += '\n'
-        # backspace
-        elif(len(backspace_search) != 0):
-            output_string += '<BSPACE>'
-        # char or numerical value
-        elif(len(value) != 0):
-            output_string += value[0]
-        else:
-            # remove new line
-            # output_string += line[25:][:-1]
-            output_string += line[25:]
-            
-    except:
-        continue
 
 print(output_string)
